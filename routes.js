64cbcfd16bd86d42c6210803a3015baeb6720b8f
poornima <pvenkatakrishnan@paypal.com>
2014-05-01 18:59:38 -0400

    Fixing feedback + changing the way routes is set up in i18n and specialization example

--- routes.js
+++ routes.js
@@ -3,4 +3,4 @@ var controllers = require('./controllers');
 module.exports = function (router) {
     router.get('/', controllers.index);
     router.get('/setLocale/:locale', controllers.setLocale);
-}
+};
