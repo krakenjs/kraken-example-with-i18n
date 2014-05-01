00191980b8ccaddaf6e74e1a59252ac856c38ffd
poornima <pvenkatakrishnan@paypal.com>
2014-04-30 20:57:08 -0400

    changes to work with latest kraken rc3

--- controllers/index.js
+++ controllers/index.js
@@ -9,10 +9,12 @@ module.exports = function (app) {
     var model = new IndexModel();
 
     app.get('/', function (req, res) {
+        console.info('***********res.locals', res.locals);
         res.render('index', model);
     });
 
     app.get('/setLocale/:locale', function (req, res) {
+        console.info('in here');
         res.cookie('locale', req.params.locale);
         res.redirect('/');
     });
